88f6c8ae0d8fd16f405557362e59316edee3d0e8
Kacper <kacpergruca@gmail.com>
2023-01-17 00:21:27 +0100

    Add simple changePersonalData - attempt with js
    Attempt to use ajax and not to reload page, but I had some issue so I've given up on the idea for now and made simple redirect

---
+++ static/script.js
@@ -0,0 +1,10 @@
+/*function getData() {
+    $.ajax({
+        url: '/get_data',
+        type: 'GET',
+        success: function(data) {
+            $('#content').html(data);
+            $('.main-title').text('Change personal Data');
+        }
+    });
+}*/
